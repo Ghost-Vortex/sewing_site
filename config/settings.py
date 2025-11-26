dcfc4437ea837f4e81de29edfbbae1bc6dd95dcf
Айман Улагашев <aymanzoubi2007@gmail.com>
2025-11-26 11:20:16 +0300

    fff

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3j982gjlyrq%6-0@(xz$3e*@mn8b$8x5ct+rfnbw!)e)4n*!we'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ulagasheff.ru",
+    "https://www.ulagasheff.ru",
+]
+
+ALLOWED_HOSTS = [
+    "ulagasheff.ru",
+    "www.ulagasheff.ru",
+    "95.163.232.54",
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
@@ -103,9 +115,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -115,8 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'   # сюда будет collectstatic класть
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
